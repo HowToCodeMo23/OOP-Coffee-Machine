c1c26796972d345849fa7e52701bdc7b64a7e4c5
Maurice Szalkai <mazou@MaZou-2.local>
2025-07-21 12:33:36 +0200

    OOP Coffee Machine

---
+++ oop-coffee-machine-start (1)/main.py
@@ -0,0 +1,46 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+
+menu = Menu()
+process = CoffeeMaker()
+money = MoneyMachine()
+
+
+end = False
+
+while end == False:
+	items = menu.get_items()         # get the items from the list 
+	print(items)
+	order = input("Which drink you wanna choose?")
+	drink = menu.find_drink(order)  # is the drink in the list ???
+	if process.is_resource_sufficient(drink):
+		cost = drink.cost   
+		print(f"{cost} €")
+		check = money.make_payment(cost)
+
+		if check == True:
+			process.make_coffee(drink)
+		else:
+			end = True
+
+
+		
+	
+	
+	  
+	
+
+
+	
+
+
+
+
+
+
+
+	
+
